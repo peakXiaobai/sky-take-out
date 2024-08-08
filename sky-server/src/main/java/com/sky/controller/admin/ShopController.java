419accb0a1f0701b7825ce4e78ba34abffc93b3e
y_peak <y_peal@qq.com>
2024-08-08 20:37:04 +0800

    基于Redis的店铺状态营业设置业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,55 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Author: Y-peak
+ * @Date: 2024/8/8 19:52 星期四
+ * @ProductName: IntelliJ IDEA
+ * @ProjectName: sky-take-out
+ * @ClassName: ShopController
+ * @Description: NOTE营业状态只有一个值，因此使用redis进行存储
+ */
+
+// NOTE 注意和user里面的ShopController同名，需要手动设置bean名称，不然会报错。默认bean为类名/方法名首字母小写
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 设置店铺的营业状态
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺的营业状态")
+    public Result<String> setStatus(@PathVariable Integer status){
+        log.info("设置店铺的营业状态为: {}", status == 1? "营业中" : "打烊中");
+        redisTemplate.opsForValue().set(KEY,status);
+        return Result.success();
+    }
+
+    /**
+     * 获取店铺的营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取店铺的营业状态为: {}", status == 1? "营业中" : "打烊中");
+        return Result.success(status);
+    }
+}

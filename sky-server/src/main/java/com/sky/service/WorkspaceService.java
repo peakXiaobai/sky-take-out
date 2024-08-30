714160a854189dbb13490c4ed81b62dc9b70c9fc
y_peak <y_peal@qq.com>
2024-08-30 13:13:20 +0800

    工作台 业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/WorkspaceService.java
@@ -0,0 +1,44 @@
+package com.sky.service;
+
+import com.sky.vo.BusinessDataVO;
+import com.sky.vo.DishOverViewVO;
+import com.sky.vo.OrderOverViewVO;
+import com.sky.vo.SetmealOverViewVO;
+import java.time.LocalDateTime;
+/**
+ * @Author: Y-peak
+ * @Date: 2024/8/30 13:06 星期五
+ * @ProductName: IntelliJ IDEA
+ * @ProjectName: sky-take-out
+ * @ClassName: WorkspaceService
+ * @Description:
+ */
+public interface WorkspaceService {
+
+    /**
+     * 根据时间段统计营业数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    BusinessDataVO getBusinessData(LocalDateTime begin, LocalDateTime end);
+
+    /**
+     * 查询订单管理数据
+     * @return
+     */
+    OrderOverViewVO getOrderOverView();
+
+    /**
+     * 查询菜品总览
+     * @return
+     */
+    DishOverViewVO getDishOverView();
+
+    /**
+     * 查询套餐总览
+     * @return
+     */
+    SetmealOverViewVO getSetmealOverView();
+
+}
